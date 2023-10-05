4c72105970f8e9615ecb516903694a59a4af8636
XFelipe41 <jfcaicedot@ut.edu.co>
2023-10-04 21:12:04 -0500

    Subida del Proyecto

---
+++ app/titulo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Titulo = () => {
+  return (
+    <div>
+      <h2 className="text-4xl text-center mt-10  p-5 font-bold text-gray-900">
+        Rick and Morty API Characters
+      </h2>
+    </div>
+  );
+};
+
+export default Titulo;
